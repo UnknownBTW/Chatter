2602597b7af77de91283d18f5edb753fadec06ad
UknownBTW <bennykhwork@gmail>
2026-03-26 22:04:06 +0000

    binary

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from Chat import main
 from pyupdater.client import Client
+from Client_config import APP_NAME, APP_VERSION, UPDATE_URLS, PUBLIC_KEY
+
+class ClientConfig(object):
+    APP_NAME = APP_NAME
+    APP_VERSION = APP_VERSION
+    UPDATE_URLS = UPDATE_URLS
+    PUBLIC_KEY = PUBLIC_KEY
+
 
 Client = Client(ClientConfig())
 app_update = Client.update_check("Chatter", "0.1.0")
